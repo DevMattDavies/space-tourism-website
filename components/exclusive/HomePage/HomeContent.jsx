0a9bbbb89bb47181e8d8681de3bc1e52daf7e512
Matt Davies <matt@mattdavies.dev>
2022-08-15 17:59:51 +0100

    Refactorted home content text

--- components/exclusive/HomePage/HomeContent.jsx
+++ components/exclusive/HomePage/HomeContent.jsx
@@ -13,9 +13,9 @@ const TextStyle = styled.p`
 function TextContent() {
   return (
     <TextStyle>
-      Let's face it; if you want to go to space, you might as well genuinely go
+      {`Let's face it; if you want to go to space, you might as well genuinely go
       to outer space and not hover kind of on the edge of it. Well sit back, and
-      relax because we'll give you a truly out of this world experience!
+      relax because we'll give you a truly out of this world experience!`}
     </TextStyle>
   );
 }
